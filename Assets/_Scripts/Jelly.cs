801a6ca91c830349ba32b54eaada08f410c63545
erinc <yigit.erinc@ozu.edu.tr>
2018-10-17 18:14:47 +0300

    prepare the project for the implementation

---
+++ Assets/_Scripts/Jelly.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jelly : MonoBehaviour {
+    [SerializeField] float velocityInX;
+    [SerializeField] float velocityInY;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(velocityInX, velocityInY);
+	}
+}
---
+++ Assets/_Scripts/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+    [SerializeField] Jelly jelly;
+    [SerializeField] Portal destination;
+    Transform jellyTransform;
+    Transform destinationTransform;
+
+    public void Start() { 
+        jellyTransform = jelly.GetComponent<Transform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.GetComponent<Jelly>() != null)
+        {
+            destinationTransform = destination.GetComponent<Transform>();
+            jellyTransform.position = destinationTransform.position;
+            jellyTransform.eulerAngles = new Vector3(jellyTransform.eulerAngles.x, jellyTransform.eulerAngles.y, destinationTransform.eulerAngles.z);
+            Vector3 velocityVector = jelly.GetComponent<Rigidbody2D>().velocity;
+            /*float velX = velocityVector.x;
+            float velY = -velocityVector.y;
+
+            jelly.GetComponent<Rigidbody2D>().velocity = new Vector3(velX, velY, 0);
+ */
+        }
+    }
+
+}
