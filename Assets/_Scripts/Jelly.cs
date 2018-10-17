44ae7f859643d58547fe31651e76562d5db14f27
CEMAL <baris.onur@ozu.edu.tr>
2018-10-17 15:13:50 +0300

    Throw sphere with 45 degrees angle and stick to square object

---
+++ Assets/_Scripts/Jelly.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jelly : MonoBehaviour {
+    [SerializeField] Vector3 vector;
+     Rigidbody myRigidbody;
+    private bool isPressedSpace = false;
+
+    private void Start() {
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.useGravity = false;
+    }
+
+    private void Update() {
+        throwJelly();
+    }
+
+    private void throwJelly() {
+        if (Input.GetKeyDown(KeyCode.Space) && isPressedSpace == false) {
+            myRigidbody.useGravity = true;
+            myRigidbody.velocity = vector;
+            isPressedSpace = true;
+        }
+    }
+}
---
+++ Assets/_Scripts/Wall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision) {
+        GameObject collider = collision.gameObject;
+
+        if (collider.GetComponent<Jelly>() != null)
+            collider.GetComponent<Rigidbody>().isKinematic = true;
+
+        collider.GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
