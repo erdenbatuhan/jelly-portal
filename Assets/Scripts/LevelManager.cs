b6669afdb99dc97a282eb3e8aa86fea3d46a6400
CEMAL <baris.onur@ozu.edu.tr>
2018-10-31 17:26:44 +0300

    Create Start Scene and required components Levels scrollable scene is also added Some icons are needed for quit/settings/back buttons in Sprite type Level mini images including 3 stars and total score and a level image or just 1-2-3.. level count are also needed in Sprite type

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    private int currentSceneIndex;
+
+	private void Start () {
+
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+	}
+
+    public void LoadSplashScene() {
+        SceneManager.LoadScene("01. Start");
+    }
+
+    public void LoadLevelsScene() {
+        SceneManager.LoadScene("02. Levels");
+    }
+    
+    public void LoadSettingsScene() {
+        SceneManager.LoadScene("03. Settings");
+    }
+
+    public void LoadNextScene() {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void ExitTheGame() {
+        Application.Quit();
+    }
+}
