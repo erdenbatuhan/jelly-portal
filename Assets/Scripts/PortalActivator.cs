7a49031ba3a6ca62b58415fdaf3a93e2961f3306
erinc <yigit.erinc@ozu.edu.tr>
2018-11-05 23:24:37 +0300

    add the missing files

---
+++ Assets/Scripts/PortalActivator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalActivator : MonoBehaviour {
+    Portal parent;
+
+    void Start()
+    {
+       parent = GetComponentInParent<Portal>();
+    }
+
+	void OnTriggerEnter2D(Collider2D collider)
+    {
+        parent.IsActive = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        parent.IsActive = false;
+    }
+}
