2dfe191823a3f4dab91f2c2827c989a755be5c46
erinc <yigit.erinc@ozu.edu.tr>
2018-11-21 14:33:20 +0300

    implement the jump token

---
+++ Assets/Scripts/JumpToken.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpToken : MonoBehaviour {
+    [SerializeField] Jelly jelly;
+
+	private void OnTriggerEnter2D(Collider2D collider) {
+        jelly.GetComponent<Rigidbody2D>().velocity = new Vector2(3, 6);
+    }
+}
